92a311ae6e714c60f67a0719e2154a0775dfd5fe
patrickpr86 <sh4dbr@gmail.com>
2020-09-18 18:55:09 -0300

    Configuring Frontent for production

--- frontend/src/pages/Charts/index.tsx
+++ frontend/src/pages/Charts/index.tsx
@@ -23,7 +23,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://dashboard.heroku.com/apps/sds1-patrick';
 
 
 const Charts = () => {
--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import {formatDate} from './helpers'
 import Pagination from './Pagination';
 import Filters from '../../components/Filters'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://dashboard.heroku.com/apps/sds1-patrick';
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
